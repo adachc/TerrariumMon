51c845e4bc89e956bd0915d47ac28b093e03742f
adachc <christofadam3003@gmail.com>
2022-01-23 18:41:55 +0000

    Initial commit

---
+++ insertMockData.js
@@ -0,0 +1,16 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("terradb");
+  for (let i = 0; i < 24; i++) {
+     
+  }
+  var myobj = [];
+  dbo.collection("terradata").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
