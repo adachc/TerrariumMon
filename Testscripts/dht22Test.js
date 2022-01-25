801aa94e833effe8c0187b421d9f85f03f23dde2
adachc <32161107+adachc@users.noreply.github.com>
2022-01-25 22:16:27 +0100

    Delete Testscripts directory

--- Testscripts/dht22Test.js
+++
@@ -1,7 +0,0 @@
-var sensor = require("node-dht-sensor");
-
-sensor.read(22, 4, function(err, temperature, humidity) {
-  if (!err) {
-    console.log(`Temperatur: ${temperature}°C, Luftfeuchtigkeit: ${humidity}%`);
-  }
-});
